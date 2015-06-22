c9552ebc83665cf3fbb7f04add4baca08f3f6596
Tiem Song <tiem@songfamily.org>
2015-06-22 10:04:45 -0700

    Add WAKE_LOCK permission to prevent phone sleeping (for track playback).  Implement play/pause functionality, including play completion and activity exit behaviors.
    Not yet implemented - scrub bar, play timers, loading UI, icon change.

--- app/src/main/java/org/songfamily/tiem/nanodegree/app1/PlayTrackActivityFragment.java
+++ app/src/main/java/org/songfamily/tiem/nanodegree/app1/PlayTrackActivityFragment.java
@@ -1,6 +1,8 @@
 package org.songfamily.tiem.nanodegree.app1;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +12,21 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 
-public class PlayTrackActivityFragment extends BaseFragment {
+
+public class PlayTrackActivityFragment extends BaseFragment
+    implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener,
+        View.OnClickListener {
     public static final String ALBUM_IMAGE_URL = "album_image_url";
     public static final String ALBUM_NAME = "album_name";
     public static final String ARTIST_NAME = "artist_name";
     public static final String TRACK_NAME = "track_name";
     public static final String TRACk_PREVIEW_URL = "track_preview_url";
+
+    private MediaPlayer mMediaPlayer = null;
+    private boolean mIsTrackPrepared = false;
+    private String mTrackPreviewUrl;
 
     public PlayTrackActivityFragment() {
     }
@@ -32,7 +42,7 @@ public class PlayTrackActivityFragment extends BaseFragment {
         String albumName = intent.getStringExtra(ALBUM_NAME);
         String artistName = intent.getStringExtra(ARTIST_NAME);
         String trackName = intent.getStringExtra(TRACK_NAME);
-        String trackPreviewUrl = intent.getStringExtra(TRACk_PREVIEW_URL);
+        mTrackPreviewUrl = intent.getStringExtra(TRACk_PREVIEW_URL);
 
         // ...and write to views...
         TextView tvAlbumName = (TextView) view.findViewById(R.id.pt_tv_album_name);
@@ -48,6 +58,64 @@ public class PlayTrackActivityFragment extends BaseFragment {
                 .load(albumImageUrl)
                 .into(ivAlbumImage);
 
+        // set up play / pause button
+        ImageView playPauseButton = (ImageView) view.findViewById(R.id.pt_iv_play_pause_track);
+        playPauseButton.setOnClickListener(this);
+
         return view;
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mMediaPlayer != null)
+            mMediaPlayer.release();
+    }
+
+    private void playTrack() {
+        if (mIsTrackPrepared) {
+            mMediaPlayer.start();
+        } else {
+            try {
+                mMediaPlayer.setDataSource(mTrackPreviewUrl);
+                mMediaPlayer.prepareAsync();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (mMediaPlayer == null)
+            initMediaPlayer();
+
+        if (mMediaPlayer.isPlaying())
+            mMediaPlayer.pause();
+        else
+            playTrack();
+    }
+
+    // *** begin media player helper methods and callbacks section ************
+    private void initMediaPlayer() {
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setOnPreparedListener(this);
+        mMediaPlayer.setOnCompletionListener(this);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mIsTrackPrepared = true;
+        playTrack();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        mIsTrackPrepared = false;
+        mMediaPlayer.release();
+        mMediaPlayer = null;
+    }
+    // *** end media player helper methods and callbacks section **************
 }
