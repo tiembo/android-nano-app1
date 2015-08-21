5e989f0c63bb823e2645cec8dda8634ba2433e97
Tiem Song <tiem@songfamily.org>
2015-08-21 13:58:57 -0700

    add broadcast receiver for menu activity to listen for track prepared / completed events and update menu items accordingly

--- app/src/main/java/org/songfamily/tiem/nanodegree/app1/BaseActivity.java
+++ app/src/main/java/org/songfamily/tiem/nanodegree/app1/BaseActivity.java
@@ -1,19 +1,35 @@
 package org.songfamily.tiem.nanodegree.app1;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.songfamily.tiem.nanodegree.app1.helpers.GlobalData;
+import org.songfamily.tiem.nanodegree.app1.helpers.PlaybackService;
 
 abstract class MenuActivity extends AppCompatActivity
     implements ArtistTracksActivityFragment.Callbacks {
+
+    protected BroadcastReceiver mBroadcastReceiver;
 
     @Override
     protected void onResume() {
         super.onResume();
         invalidateOptionsMenu();
+        mBroadcastReceiver = initBroadcastReceiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver,
+                new IntentFilter(PlaybackService.SERVICE_FILTER));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
     }
 
     @Override
@@ -49,4 +65,21 @@ abstract class MenuActivity extends AppCompatActivity
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    private BroadcastReceiver initBroadcastReceiver() {
+        return new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String message = intent.getStringExtra(PlaybackService.SERVICE_MESSAGE);
+
+                switch (message) {
+                    case PlaybackService.TRACK_PREPARED:
+                    case PlaybackService.TRACK_COMPLETED:
+                        invalidateOptionsMenu();
+                        break;
+                }
+            }
+        };
+    }
+
 }
